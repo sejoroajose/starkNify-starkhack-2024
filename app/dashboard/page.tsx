12aa7bed81ccaf7dc9e9a33db07e2efd200c162a
sejoroajose <53483859+sejoroajose@users.noreply.github.com>
2024-06-16 05:29:01 +0100

    adding the dashboard page where the application shell will be built

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import WalletConnect from '@/app/dynamic'
+
+export default function Dashboard() {
+  return (
+    <>
+      <p>Welcome to the dashboard Page</p>
+    </>
+  )
+}
